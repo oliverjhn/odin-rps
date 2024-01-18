050e5df68e1086ae913f530a37e5a46167d9d2c0
Oliver Hogan <oliver@oliver-hogan.com>
2024-01-18 16:00:22 +1100

    fix bugs and finish

--- js/script.js
+++ js/script.js
@@ -24,11 +24,11 @@ function playRound(playerChoice, computerChoice) {
     if (playerChoice != computerChoice) {
         switch (playerChoice) {
             case "ROCK":
-                return computerChoice === "SCISSORS" ? "WIN" : "LOSE";
+                return computerChoice === "SCISSORS" ? "WIN" : "LOSS";
             case "PAPER":
-                return computerChoice === "ROCK" ? "WIN" : "LOSE";
+                return computerChoice === "ROCK" ? "WIN" : "LOSS";
             case "SCISSORS":
-                return computerChoice === "PAPER" ? "WIN" : "LOSE";
+                return computerChoice === "PAPER" ? "WIN" : "LOSS";
             default:
                 console.error("Something went wrong with the second switch statement");
                 return;
@@ -37,16 +37,36 @@ function playRound(playerChoice, computerChoice) {
     return "TIE";
 }
 
-playGame() {
+function playGame() {
     let playerScore = 0;
     let computerScore = 0;
 
     while ((playerScore + computerScore) < 5) {
-        let playerChoice = prompt("Rock, paper, or scissors? ");
+        let playerChoice = prompt("ROCK, PAPER, OR SCISSORS? ").toUpperCase();
         let computerChoice = getComputerChoice();
-        let result = playRound(playerChoice, computerScore);
+        let result = playRound(playerChoice, computerChoice);
 
-        console.log("The computer went " + computerChoice + "and you went " + playerChoice + ", the result was a " + result + "\n");
+        console.log("The computer went " + computerChoice + " and you went " + playerChoice + ", the result was a " + result);
+        switch (result) {
+            case "WIN":
+                playerScore += 1;
+                break;
+            case "LOSS":
+                computerScore += 1;
+                break;
+            default:
+                break;
+        }
+
+        if ((playerScore + computerScore) != 5) {
+            console.log("The score is " + playerScore + " to " + computerScore);
+        }
+    }
+
+    if (playerScore > computerScore) {
+        console.log("YOU WIN! The final score was " + playerScore + " to " + computerScore);
+    } else {
+        console.log("You lost, sad. The final score was " + playerScore + " to " + computerScore);
     }
 }
 
